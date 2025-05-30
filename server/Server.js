a20de145245ca41970b1619e53a116f98ec1ceda
gaeun2o <162069275+gaeun2o@users.noreply.github.com>
2025-05-30 17:10:33 +0900

    ch.14/server
    11주차 실습입니당

--- server/Server.js
+++ server/Server.js
@@ -7,6 +7,9 @@ const db = require('node-mysql/lib/db');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+//정적 파일 라이브러리 추가
+app.use(express.static('public'));
+//몽고DB 라이브러리 추가
 const mongoclient = require('mongodb').MongoClient;
 const ObjId = require('mongodb').ObjectId;
 const url = 'mongodb+srv://gaeunpop:1111@cluster0.uqxyg33.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
@@ -24,7 +27,7 @@ let mydb;
 
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.render('index.ejs');
 });
 
 
@@ -43,6 +46,22 @@ app.get('/list', async function(req, res){
 app.get('/enter', function(req, res){
     res.render('enter.ejs');
 });
+
+
+
+//'/content'요청에 대한 처리 루틴
+app.get('/content/:id', function(req, res){
+    console.log(req.params.id);
+    req.params.id = new ObjId(req.params.id);
+    mydb
+        .collection('post')
+        .findOne({_id: req.params.id})
+        .then(result => {
+            console.log(result);
+            res.render('content.ejs', {data: result});
+        });
+});
+
 //'save'요청에 대한 post 방식의 처리 루틴
 app.post('/save', function(req, res){
     console.log(req.body.title);
@@ -56,12 +75,41 @@ app.post('/save', function(req, res){
         console.log('데이터 추가 성공');
     });
 
-    res.send('데이터 추가 성공');
+    res.redirect('/list');
+});
+
+//'edit'요청에 대한 처리 루틴(get방식)
+app.get('/edit/:id', function(req, res){
+    req.params.id = new ObjId(req.params.id);
+    mydb
+        .collection('post')
+        .findOne({_id: req.params.id})
+        .then(result => {
+            console.log(result);
+            res.render('edit.ejs', {data: result});
+        });
+});
+
+//'edit'요청에 대한 처리 루틴(post방식)
+app.post("/edit", function(req, res){
+    console.log(req.body);
+    req.body.id = new ObjId(req.body.id);
+    mydb
+        .collection('post')
+        .updateOne({_id: req.body.id},
+            {$set : {title:req.body.title, content:req.body.content, date:req.body.someDate}})
+        .then(result => {
+            console.log("수정 완료");
+            res.redirect('/list');
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 app.post('/delete', function(req,res){
-    console.log(req.body._id);
-    req.body._id=new ObjId(req.body._id);
+    console.log(req.body.id);
+    req.body.id=new ObjId(req.body.id);
     mydb.collection('post').deleteOne(req.body)
     .then(result=>{
         console.log('삭제완료');
